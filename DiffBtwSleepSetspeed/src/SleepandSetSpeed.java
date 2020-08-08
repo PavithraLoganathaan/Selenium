2d761eccb6b842e984bb69308dbffe9fb64ab022
Boopathi <Boopathi@BoopathiPC>
2020-08-08 18:33:44 +0530

    Initial Commit

---
+++ DiffBtwSleepSetspeed/src/SleepandSetSpeed.java
@@ -0,0 +1,24 @@
+
+public class SleepandSetSpeed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Thread.sleep();
+		 * 
+		 * It has to be given before each operations
+		 * 
+		 * Thread.sleep(); operation1; Thread.sleep(); operation2;
+		 * 
+		 * 
+		 * 
+		 * Selenium.setSpeed();
+		 * 
+		 * one time is enough . but this method is not available now
+		 * 
+		 * selenium.setSpeed(); operation1; operation2;
+		 */
+
+	}
+
+}
